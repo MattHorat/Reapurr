b7c146d52b8fc4b41de2104f10c6370fddb0df39
Imaboat <ezy453@gmail.com>
2017-01-21 00:08:45 +1100

    InitialLockSystem

---
+++ Assets/LockSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockSystem : MonoBehaviour {
+
+    public Image[] lockImages;
+    private int count;
+    List<int> objectNumbers = new List<int>();
+
+
+    public void ClickObject(int objectID)
+    {
+        lockImages[count].color = Color.green;
+        count++;
+        objectNumbers.Add(objectID);
+    }
+
+    public void ClickCheck()
+    {
+        foreach (int objectID in objectNumbers)
+        {
+
+        }
+    }
+}
