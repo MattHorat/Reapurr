5014a15ab8fabf8f05cd9eb59f7015a81a78338f
MattHorat <matthorat82@hotmail.com>
2017-01-21 01:45:38 +1100

    Added attractors

---
+++ Assets/Code/Attractor/SoundRotationAttractor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SoundRotationAttractor : MonoBehaviour {
+    private void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attract();
+        }
+    }
+
+    public void Attract()
+    {
+        Human[] humans = GameObject.FindObjectsOfType<Human>();
+        foreach(Human human in humans)
+        {
+            human.FaceTarget(gameObject);
+        }
+    }
+}
--- Assets/Code/Human/Human.cs
+++ Assets/Code/Human/Human.cs
@@ -14,13 +14,9 @@ public class Human : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SetMoveTarget(targetObject);
-        }
     }
 
-    public void SetMoveTarget(GameObject targetObject)
+    private bool HasLineOfSight(GameObject targetObject)
     {
         Vector3 start = transform.position;
         Vector3 direction = (targetObject.transform.position - transform.position).normalized;
@@ -31,7 +27,35 @@ public class Human : MonoBehaviour {
         {
             if (sightTest.collider.gameObject != gameObject && targetObject == sightTest.collider.gameObject)
             {
-                targetPosition = targetObject.transform.position;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void SetMoveTarget(GameObject targetObject)
+    {
+        if (HasLineOfSight(targetObject))
+        {
+            targetPosition = targetObject.transform.position;
+        }
+    }
+
+    public void FaceTarget(GameObject targetObject)
+    {
+        if (HasLineOfSight(targetObject))
+        {
+            Vector2 localPositionTarget = targetObject.transform.position - gameObject.transform.position;
+            // If we are closer to the x axis, face based on x, otherwise y
+            if (Mathf.Abs(localPositionTarget.x) > Mathf.Abs(localPositionTarget.y))
+            {
+                float rotation = (localPositionTarget.x > 0) ? 90 : -90;
+                gameObject.GetComponent<Rigidbody2D>().MoveRotation(rotation);
+            }
+            else
+            {
+                float rotation = (localPositionTarget.y > 0) ? 180 : -180;
+                gameObject.GetComponent<Rigidbody2D>().MoveRotation(rotation);
             }
         }
     }
