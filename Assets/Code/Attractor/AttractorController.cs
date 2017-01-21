407a97bf4842c4459c7b387a3e72957fb9db0881
Matt Horat <matthorat82@hotmail.com>
2017-01-21 11:42:27 +1100

    things are broken

--- Assets/Code/Attractor/AttractorController.cs
+++ Assets/Code/Attractor/AttractorController.cs
@@ -6,20 +6,13 @@ using UnityEngine.UI;
 public class AttractorController : MonoBehaviour {
 
     public bool hasBeenSelected;
-    private GameObject mainScreen;
     public string objectName;
-
-    private void Start()
-    {
-        mainScreen = GameObject.FindGameObjectWithTag("MainScreen");
-    }
-
 
     public void InteractAttractor()
     {
         if (!hasBeenSelected)
         {
-            mainScreen.GetComponentInChildren<MainUI>().AssignAttractor(gameObject);
+            GameObject.FindGameObjectWithTag("MainScreen").GetComponentInChildren<MainUI>().AssignAttractor(gameObject);
             FindObjectOfType<ActionQueue>().AddAction(GetComponent<Actionable>());
             hasBeenSelected = true;
         }
